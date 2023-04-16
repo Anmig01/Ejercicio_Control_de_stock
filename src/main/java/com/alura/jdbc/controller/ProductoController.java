47d3668048a5e346965477faf0a1aa3ed666bb11
Miguel <fou14.mf@gmail.com>
2023-04-16 16:05:14 -0500

    Agregando las opciones de guardar, eliminar y visualizar los dastos en la interfaz grafica

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -1,39 +1,84 @@
 package com.alura.jdbc.controller;
 
-import java.sql.Statement;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.alura.jdbc.factory.ConnectionFactory;
 
 public class ProductoController {
 
-	public void modificar(String nombre, String descripcion, Integer id) {
+	public void modificar(String nombre, String descripcion, Integer id) throws SQLException {
 		// TODO
-	}
-
-	public void eliminar(Integer id) {
-		// TODO
-	}
-
-	public List<?> listar() throws SQLException {
-		// TODO
-		Connection con = DriverManager.getConnection(
-				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-				"root",
-				"Nasuligi01");
+		Connection con = new ConnectionFactory().recuperaConexion();
 		Statement statement = con.createStatement();
 		
-		boolean result = statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+		statement.execute("UPDATE producto SET NOMBRE = '" + nombre + "', "
+				+ "DESCRIPCION = '" + descripcion + "' WHERE id = " + id );
 		
-		System.out.println(result);
-		con.close();
-		return new ArrayList<>();
+		
 	}
 
-    public void guardar(Object producto) {
+	public int eliminar(Integer id) throws SQLException {
 		// TODO
+		Connection con = new ConnectionFactory().recuperaConexion();
+		Statement statement = con.createStatement();
+		statement.execute("DELETE FROM PRODUCTO WHERE ID = " + id);
+		
+		return statement.getUpdateCount();
+		
+		
+	}
+
+	public List<Map<String, String>> listar() throws SQLException {
+		// TODO
+		Connection con = new ConnectionFactory().recuperaConexion();
+		Statement statement = con.createStatement();
+		
+		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+		
+		ResultSet resultSet = statement.getResultSet();
+		//List<Map<String,String>> resultado = new ArrayList<>();
+		List<Map<String,String>> resultado = new ArrayList<>();
+		
+		while (resultSet.next()) {
+			Map<String, String> fila = new HashMap<>();
+			
+			fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+			fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+			fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+			fila.put("CANTIDAD", String.valueOf(resultSet.getString("CANTIDAD")));
+			
+			resultado.add(fila);
+			
+		}
+		
+		//System.out.println(result);
+		con.close();
+		
+		
+		return resultado;
+	}
+
+    public void guardar(Map<String, String> producto) throws SQLException {
+		// TODO
+    	Connection con = new ConnectionFactory().recuperaConexion();
+    	Statement statement = con.createStatement();
+    	statement.execute("INSERT INTO PRODUCTO (nombre, descripcion, cantidad) "
+    			+ "VALUES ('"+ producto.get("NOMBRE") + "', '"
+    			+ producto.get("DESCRIPCION") + "',"
+    			+ producto.get("CANTIDAD")+");", Statement.RETURN_GENERATED_KEYS);
+    	
+    	ResultSet resultSet = statement.getGeneratedKeys();
+    	while (resultSet.next()) {
+    		System.out.println(String.format("Fue insertado el producto con el ID %d", resultSet.getInt(1)));
+			
+		}
 	}
 
 }
